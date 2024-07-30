6cb95f97a93b091937fc9b3fbc55fd222689688a
JonneLameu <joaovictorlameu@hotmail.com.br>
2024-07-30 16:42:50 -0300

    first commit

---
+++ servicos.php
@@ -0,0 +1,67 @@
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="UTF-8">
+
+<title>Serviços</title>
+
+
+<html lang="br">
+<head>
+<link rel="icon" href="img/Logo2.png" sizes="16x16 32x32" type="image/png"> 
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+ 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet"  href="style.css" type="text/css" >
+<link rel="stylesheet" id="themify-media-queries-css" href="media-queries.css" type="text/css" media="all">
+
+
+	
+		<header id="header" class="pagewidth">
+			
+					
+				
+				
+				
+				<h2>	CRIAÇÃO DE SITES </H2>
+				<h3><BR><P>Sua empresa não precisa de 'alguém para fazer o site'. Sua empresa precisa que todo o marketing comunique bem para vender mais e melhor. Onde todas as peças de comunicação tenham a mesma linguagem, inclusive o site. 
+A CRIAÇÃO DE SITES NA ARTS PRODUÇÕES É UM PROCESSO QUE 
+ENVOLVE PROFISSIONAIS DE DIFERENTES ESPECIALIDADES</H3></BR></P>
+
+<br><H3><P>A Arts Produções atua na criação de sites com foco na usabilidade e sempre com interfaces bonitas e simples, fáceis de navegar, com excelente design e bem construídas. Utilizamos as tecnologias mais recentes e indicamos a melhor solução para cada projeto, sempre considerando os objetivos do cliente. O nosso processo de criação de sites foca a união do design com a tecnologia desde a consultoria inicial, o que garante sites leves, bonitos e de perfeito funcionamento. 
+DESENVOLVIMENTO WEB NO PONTO ONDE DESIGN E
+TECNOLOGIA SE ENCONTRAM.</br></H3></p>
+<br><H2>Manutenção de sites</H2></br>
+<br><P><H3><P>Tão importante quanto ter um bom site é mante-lo atualizado, gerando negócios e representando de forma adequada sua empresa. Pensando nisso nossa empresa criou um departamento para cuidar do seu site a partir do momento que ele é publicado.
+Acreditamos que é neste momento que o site começa a trabalhar, ou seja, fazer o site, é o primeiro passo, o que vai ser feito depois que determina o sucesso do projeto. Em nossa equipe de atualização e suporte você conta com uma equipe completa de:</H3></br></P>
+<br><H3>•	Web designers</br></H3>
+<br><H3>•	Publicitários</br></H3>
+<br><H3>•	Designers</br></H3>
+<br><H3>•	Técnicos em Informática</br></H3>
+<br><H3>•	Programadores</br></H3>
+
+<br><H2> BRADING </BR></H2>
+<br><H3><P>Branding é a construção e o fortalecimento de uma marca. 
+A Arts Produções desenvolveu uma metodologia própria para seus processos de branding, focada em criar uma conexão entre marca e consumidor e proporcionar maior fidelidade do público. O trabalho consiste em analisar o perfil da empresa e do público alvo para identificar e diagnosticar o melhor caminho para construir uma marca forte e eficaz.</br></H3></P>
+
+<br><h2> 	HOSTSITES </br></h2>
+<P><br><H3>Os hotsites são sites de promoção. Geralmente contam com um período pré-definido para estarem ativos, mas nada impede também de se tornarem constantes. Muito usados para eventos, lançamentos de produtos e novidades, os hotsites têm maior apelo visual, são diretos e funcionais. Para atingir seus objetivos de marketing com a criação do seu próximo hotsite, conte com a Arts Produções!</P></br></H3>
+				
+				
+				 <div class="col-sm-8">
+      
+    </div>
+  
+
+		
+		
+				
+
+
+
+
+</body></html>
